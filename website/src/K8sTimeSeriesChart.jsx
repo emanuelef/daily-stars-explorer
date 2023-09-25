b6cd165a2e2251ad34386de970a421948da47e9f
Emanuele Fumagalli <emanuelef@gmail.com>
2023-09-25 14:55:39 +0100

    test change on website not to trigger build

--- website/src/K8sTimeSeriesChart.jsx
+++ website/src/K8sTimeSeriesChart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import TextField from "@mui/material/TextField";
