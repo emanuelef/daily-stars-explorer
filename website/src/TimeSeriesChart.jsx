b6b7f480cffc7a618c55df56bd6042704456b8eb
Emanuele Fumagalli <emanuelef@users.noreply.github.com>
2023-09-29 08:19:37 +0100

    Update fusionchart (#2)

--- website/src/TimeSeriesChart.jsx
+++ website/src/TimeSeriesChart.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
+import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
@@ -8,6 +9,26 @@ import FusionCharts from "fusioncharts";
 import TimeSeries from "fusioncharts/fusioncharts.timeseries";
 import ReactFC from "react-fusioncharts";
 import schema from "./schema";
+import EstimatedTimeProgress from "./EstimatedTimeProgress";
+
+const HOST = import.meta.env.VITE_HOST;
+
+const parseGitHubRepoURL = (url) => {
+  // Define the regular expression pattern to match GitHub repository URLs
+  const repoURLPattern =
+    /^(?:https?:\/\/github.com\/)?(?:\/)?([^/]+)\/([^/]+)$/;
+
+  // Use RegExp.exec to match the pattern against the URL
+  const match = repoURLPattern.exec(url);
+
+  if (match && match.length === 3) {
+    const owner = match[1];
+    const repoName = match[2];
+    return `${owner}/${repoName}`;
+  } else {
+    return null; // Invalid URL
+  }
+};
 
 ReactFC.fcRoot(FusionCharts, TimeSeries);
 const chart_props = {
@@ -30,119 +51,67 @@ const chart_props = {
       ],
       chart: {
         animation: "0",
+        theme: "candy",
       },
     },
   },
 };
-const API_URL =
-  "https://raw.githubusercontent.com/emanuelef/cncf-repos-stats/main/stars-history-30d.json";
 
-const FULL_URL_CSV =
-  "https://raw.githubusercontent.com/emanuelef/github-repo-activity-stats/main/all-stars-k8s.csv";
-
-const CSVToArray = (data, delimiter = ",", omitFirstRow = true) =>
-  data
-    .slice(omitFirstRow ? data.indexOf("\n") + 1 : 0)
-    .split("\n")
-    .map((v) => {
-      let arr = v.split(delimiter);
-      arr[1] = parseInt(arr[1]);
-      arr[2] = parseInt(arr[2]);
-      return arr;
-    });
-
-const movingAvg = (array, countBefore, countAfter = 0) => {
-  const result = [];
-  for (let i = 0; i < array.length; i++) {
-    const subArr = array.slice(
-      Math.max(i - countBefore, 0),
-      Math.min(i + countAfter + 1, array.length)
-    );
-    const avg =
-      subArr.reduce((a, b) => a + (isNaN(b) ? 0 : b), 0) / subArr.length;
-    result.push(avg);
-  }
-  return result;
-};
-
-function TimeSeriesChart({ repo }) {
+function TimeSeriesChart() {
   const [ds, setds] = useState(chart_props);
+  const [selectedRepo, setSelectedRepo] = useState("helm/helm-mapkubeapis");
   const [selectedValue, setSelectedValue] = useState("increment");
+  const [result, setResult] = useState([]);
 
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
-
-  const loadData = async () => {
-    try {
-      /*
-      const response = await fetch(FULL_URL_CSV);
-      const res = await response.text();
-      const data = CSVToArray(res);
-      console.log(data);
-      */
-
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      const dataRepo = data[repo];
-
-      let calcMovingAvg = dataRepo.map((el) => {
-        return el[1];
+  const fetchRepoStats = (repo) => {
+    console.log(repo);
+    fetch(`${HOST}/allStars?repo=${repo}`)
+      .then((response) => {
+        // Check if the response status indicates success (e.g., 200 OK)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        // Attempt to parse the response as JSON
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setResult(data);
+        const fusionTable = new FusionCharts.DataStore().createDataTable(
+          data,
+          schema
+        );
+        const options = { ...ds };
+        options.timeseriesDs.dataSource.data = fusionTable;
+        options.timeseriesDs.dataSource.yAxis[0].plot[0].value =
+          "Cumulative Stars";
+        setds(options);
+      })
+      .catch((e) => {
+        console.error(`An error occurred: ${e}`);
       });
-      calcMovingAvg = movingAvg(calcMovingAvg, 3, 3);
-
-      const movingAverageData = dataRepo.map((el, index) => {
-        el[1] = calcMovingAvg[index];
-        return el;
-      });
-
-      console.log(movingAverageData);
-
-      const fusionTable = new FusionCharts.DataStore().createDataTable(
-        movingAverageData,
-        schema
-      );
-      const options = { ...ds };
-      options.timeseriesDs.dataSource.data = fusionTable;
-      options.timeseriesDs.dataSource.yAxis[0].plot[0].value =
-        selectedValue === "increment" ? "New Stars" : "Cumulative Stars";
-      setds(options);
-    } catch (err) {
-      console.log(err);
-    }
   };
 
   useEffect(() => {
-    console.log("render");
-    loadData();
-  }, [repo, selectedValue]);
+    fetchRepoStats(selectedRepo);
+  }, []);
+
+  const handleInputChange = (event, setStateFunction) => {
+    const inputText = event.target.value;
+    setStateFunction(inputText);
+    fetchRepoStats(parseGitHubRepoURL(inputText));
+  };
 
   return (
     <div>
-      <FormControl
-        component="fieldset"
-        style={{ marginTop: 20, marginLeft: 20 }}
-      >
-        <FormLabel component="legend">Select one option:</FormLabel>
-        <RadioGroup
-          aria-label="options"
-          name="options"
-          value={selectedValue}
-          onChange={handleChange}
-          row
-        >
-          <FormControlLabel
-            value="increment"
-            control={<Radio />}
-            label="Stars per day"
-          />
-          <FormControlLabel
-            value="cumulative"
-            control={<Radio />}
-            label="Cumulative stars"
-          />
-        </RadioGroup>
-      </FormControl>
+      <TextField
+        style={{ marginTop: "20px", marginRight: "20px", marginLeft: "20px" }}
+        label="Enter a GitHub repository"
+        variant="outlined"
+        value={selectedRepo}
+        onChange={(e) => handleInputChange(e, setSelectedRepo)}
+      />
+      <EstimatedTimeProgress text="Estimated Time Left" totalTime={36} />
       <ReactFC {...ds.timeseriesDs} />
     </div>
   );
