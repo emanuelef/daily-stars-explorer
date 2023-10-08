1a18ed1d85f4a94e2d65e6432fec0f294c5950d8
Emanuele Fumagalli <emanuelef@gmail.com>
2023-10-08 08:12:28 +0100

    Avoid getting all stats when fectching all stars

--- main.go
+++ main.go
@@ -214,7 +214,7 @@ func main() {
 
 		onGoingStars[repo] = true
 
-		result, err := client.GetAllStats(ctx, repo)
+		_, createdAt, err := client.GetTotalStars(ctx, repo)
 		if err != nil {
 			log.Printf("Error getting all stats %v", err)
 			return c.Status(404).SendString("Custom 404 Error: Resource not found")
@@ -228,7 +228,7 @@ func main() {
 
 		go func() {
 			defer wg.Done()
-			allStars, _ = client.GetAllStarsHistory(ctx, repo, result.CreatedAt, updateChannel)
+			allStars, _ = client.GetAllStarsHistory(ctx, repo, createdAt, updateChannel)
 		}()
 
 		for progress := range updateChannel {
