72166ededab310361f246e2f82fbca04efb7c51b
Emanuele Fumagalli <emanuelef@gmail.com>
2023-10-20 20:10:16 +0100

    remove empty spaces from input

--- website/src/githubUtils.js
+++ website/src/githubUtils.js
@@ -1,18 +1,19 @@
 const parseGitHubRepoURL = (url) => {
-    // Define the regular expression pattern to match GitHub repository URLs
-    const repoURLPattern =
-      /^(?:https?:\/\/github.com\/)?(?:\/)?([^/]+)\/([^/]+)$/;
-  
-    // Use RegExp.exec to match the pattern against the URL
-    const match = repoURLPattern.exec(url);
-  
-    if (match && match.length === 3) {
-      const owner = match[1];
-      const repoName = match[2];
-      return `${owner}/${repoName}`;
-    } else {
-      return null; // Invalid URL
-    }
-  };
-  
-  export { parseGitHubRepoURL };
+  url = url.replace(" ", "");
+  // Define the regular expression pattern to match GitHub repository URLs
+  const repoURLPattern =
+    /^(?:https?:\/\/github.com\/)?(?:\/)?([^/]+)\/([^/]+)$/;
+
+  // Use RegExp.exec to match the pattern against the URL
+  const match = repoURLPattern.exec(url);
+
+  if (match && match.length === 3) {
+    const owner = match[1];
+    const repoName = match[2];
+    return `${owner}/${repoName}`;
+  } else {
+    return null; // Invalid URL
+  }
+};
+
+export { parseGitHubRepoURL };
