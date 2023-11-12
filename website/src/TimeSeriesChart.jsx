a726be905b356a1b587a4309ba918faf432c9bfb
Emanuele Fumagalli <emanuelef@gmail.com>
2023-11-12 19:47:45 +0000

    Preparing for adding new fields

--- website/src/TimeSeriesChart.jsx
+++ website/src/TimeSeriesChart.jsx
@@ -378,9 +378,9 @@ function TimeSeriesChart() {
         <TextField
           style={{
             marginTop: "20px",
-            marginRight: "20px",
+            marginRight: "10px",
             marginLeft: "10px",
-            width: "100px",
+            width: "90px",
           }}
           size="small"
           id="total-stars"
@@ -393,7 +393,7 @@ function TimeSeriesChart() {
         <TextField
           style={{
             marginTop: "20px",
-            marginRight: "20px",
+            marginRight: "10px",
             marginLeft: "10px",
             width: "200px",
           }}
@@ -410,7 +410,7 @@ function TimeSeriesChart() {
             marginTop: "20px",
             marginRight: "20px",
             marginLeft: "10px",
-            width: "150px",
+            width: "120px",
           }}
           size="small"
           id="age"
