447d3e8cc8ae9a2d02aa18e44f49c5ab6ac5b180
Emanuele Fumagalli <emanuelef@users.noreply.github.com>
2024-01-27 17:31:01 +0000

    Update get_repos_data.sh (#70)

--- scripts/get_repos_data.sh
+++ scripts/get_repos_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 while IFS= read -r repo; do
-  # Construct the URL and perform the GET request
   url="http://143.47.226.125:8080/allStars?repo=$repo"
-  response=$(curl -sI "$url") # Use -I to get only the headers
-  status_code=$(echo "$response" | grep -i "HTTP" | cut -d ' ' -f 2)
+
+  response=$(curl -s "$url")  # Fetch the full response
+  status_code=$(echo "$response" | head -n 1 | grep -i "HTTP" | cut -d ' ' -f 2)
 
   if [ -n "$status_code" ]; then
-    json_lines=$(curl -s "$url" | jq -c '.[]' | wc -l)
+    json_lines=$(echo "$response" | jq -c '.[]' | wc -l)
     echo "Repo: $repo"
     echo "Status Code: $status_code"
     echo "Number of Lines in JSON Array: $json_lines"
