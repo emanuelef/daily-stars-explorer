bb4f96ff6e8567df0700c2d996e6c3e090ed9b6f
Emanuele Fumagalli <emanuelef@gmail.com>
2024-03-02 21:19:26 +0000

    make parsing repo from url less strict

--- website/src/githubUtils.js
+++ website/src/githubUtils.js
@@ -7,7 +7,7 @@ const parseGitHubRepoURL = (url) => {
   url = stripTrailingSlash(url);
   // Define the regular expression pattern to match GitHub repository URLs
   const repoURLPattern =
-    /^(?:https?:\/\/github.com\/)?(?:\/)?([^/]+)\/([^/]+)$/;
+    /^(?:https?:\/\/github\.com\/)?(?:\/)?([^/]+)\/([^/]+)(?:\/.*)?$/;
 
   // Use RegExp.exec to match the pattern against the URL
   const match = repoURLPattern.exec(url);
