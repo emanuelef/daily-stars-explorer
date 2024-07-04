74289588db794efdcc071c44835a3d52c318727a
Emanuele Fumagalli <emanuelef@gmail.com>
2024-07-04 11:22:35 +0100

    fix semconv version

--- otel_instrumentation/opentelemetry_setup.go
+++ otel_instrumentation/opentelemetry_setup.go
@@ -13,7 +13,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 // Used to initialise the global OpenTelemetry trace provider and exporter
