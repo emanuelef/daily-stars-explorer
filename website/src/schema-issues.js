5f6c0cf373a2108f21f034ca3928b62404294303
Emanuele Fumagalli <emanuelef@users.noreply.github.com>
2024-07-14 21:06:44 +0100

    Trying to put open and closed in same chart (#163)
    * Trying to put open and closed in same chart
    
    * fix
    
    * Updated Issues Graphs

--- website/src/schema-issues.js
+++ website/src/schema-issues.js
@@ -13,19 +13,7 @@ const schema = [
     type: "number",
   },
   {
-    name: "Total Opened",
-    type: "number",
-  },
-  {
-    name: "Total Closed",
-    type: "number",
-  },
-  {
-    name: "Currently Open",
-    type: "number",
-  },
-  {
-    name: "Total Currently Open",
+    name: "Open Issues",
     type: "number",
   },
 ];
