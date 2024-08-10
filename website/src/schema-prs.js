50862a5ac820f588c21ea10b59e071ec42623512
Emanuele Fumagalli <emanuelef@gmail.com>
2024-08-10 22:01:12 +0100

    Added UI for PRs history

---
+++ website/src/schema-prs.js
@@ -0,0 +1,25 @@
+const schema = [
+  {
+    name: "Time",
+    type: "date",
+    format: "%-d-%-m-%Y",
+  },
+  {
+    name: "Daily Opened",
+    type: "number",
+  },
+  {
+    name: "Daily Merged",
+    type: "number",
+  },
+  {
+    name: "Daily Closed",
+    type: "number",
+  },
+  {
+    name: "Open PRs",
+    type: "number",
+  },
+];
+
+export default schema;
