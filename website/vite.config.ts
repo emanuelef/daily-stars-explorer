2986f35443f491cb9a7881581881df0675a07e95
Emanuele Fumagalli <emanuelef@gmail.com>
2026-03-14 06:07:50 +0000

    chore: UI update deps

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -6,4 +6,12 @@ export default defineConfig({
   base: '/daily-stars-explorer/',
   plugins: [react()],
   assetsInclude: ["**/*.md"],
+  resolve: {
+    alias: {
+      "react-fusioncharts": "react-fusioncharts/dist/react-fusioncharts.js",
+    },
+  },
+  optimizeDeps: {
+    include: ["fusioncharts", "react-fusioncharts"],
+  },
 })
